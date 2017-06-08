c6d56bcae73e48de36e86f6bd69f00f1dcbe10d4
jgrandydev <jgrandydev@github.com>
2017-06-07 22:10:13 -0700

    added test project + test

---
+++ PaketSandbox.Universe.Tests/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using PaketSandbox.Universe;
+
+namespace PaketSandbox.Universe.Tests
+{
+    public class UnitTests
+    {
+        [Test]
+        public void AnswerToUniverse()
+        {
+            Assert.IsTrue(Universe.Answer()  == 42);
+        }
+    }
+}
